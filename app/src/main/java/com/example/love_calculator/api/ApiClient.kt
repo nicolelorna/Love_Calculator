9b9cfa0ff4e3f00178b3c6808e5fef5c4e85bbbd
nicolelorna <nicolelornah02@gmail.com>
2020-11-13 19:09:59 +0300

    Calculate Love

--- app/src/main/java/com/example/love_calculator/api/ApiClient.kt
+++ app/src/main/java/com/example/love_calculator/api/ApiClient.kt
@@ -1,4 +1,19 @@
-package com.example.love_calculator.api
+package com.example.lovecalculator.api
 
-class ApiClient {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    val client=OkHttpClient.Builder().build()
+    var retrofit= Retrofit.Builder()
+        .baseUrl("https://love-calculator.p.rapidapi.com/getPercentage?")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> buildService(service:Class<T>): T {
+        return retrofit.create(service)
+    }
+
 }
--- app/src/main/java/com/example/love_calculator/api/ApiInterface.kt
+++ app/src/main/java/com/example/love_calculator/api/ApiInterface.kt
@@ -1,4 +1,18 @@
-package com.example.love_calculator.api
+package com.example.lovecalculator.api
+
+import com.example.lovecalculator.model.CalculationResponse
+import com.example.lovecalculator.model.RequestResponse
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ApiInterface {
+    @GET("percentage")
+    fun requestPercentage(@Header("Authorization") accessToken: RequestBody):retrofit2.Call<RequestResponse>
+
+    @POST("response")
+    fun getResponse(@Body requestBody: RequestBody):retrofit2.Call<CalculationResponse>
 }
+
